814c229c97d18a7316711cd37c35c7971c1d1843
Chidi Christopher <128011857+ChidiChris@users.noreply.github.com>
2024-01-28 18:12:14 -0800

    Update music.js

--- music.js
+++ music.js
@@ -46,7 +46,9 @@ listBox.addEventListener("click", function(e){
 //     localStorage.setItem("data", listBox.innerHTML);
 // }
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  const inputBox = document.getElementById('input-box');
+  const listBox = document.getElementById('list-box');
 
   // Load tasks from local storage
   const savedTasks = localStorage.getItem('tasks');
