79f44fac9287a78a846199d09eab9e3c9d4fc3d8
Chidi Christopher <128011857+ChidiChris@users.noreply.github.com>
2024-02-20 00:28:27 -0800

    Update music.js

--- music.js
+++ music.js
@@ -20,7 +20,7 @@ function addTask(){
         listBox.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
-        li.appendChild(span)
+        li.appendChild(span);
     }
     inputBox.value = ""
     saveData();
@@ -31,20 +31,16 @@ function addTask(){
 listBox.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        saveData();
+        // saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        saveData();
+        // saveData();
     }
 }, false)
 
 //This next function helps you to store the task on your browser.
 //The task will still be visible even after you refresh your browser.
-
-// function saveData(){
-//     localStorage.setItem("data", listBox.innerHTML);
-// }
 
 document.addEventListener('DOMContentLoaded', () => {
   const inputBox = document.getElementById('input-box');
@@ -98,24 +94,6 @@ function showTask(){
 }
 saveData();
 
-// const song = document.querySelector('.song');
-// const navLinks = document.querySelector('.nav-links');
-
-// menu.addEventListener('click', () => {
-//     navLinks.classList.toggle('isActive');
-// });
-
-
-const audioPlayer = document.querySelector("#audio-player");
-const prevButton  = document.querySelector("#prev-button");
-const playPauseButton  = document.querySelector("#play-pause-button")
-const nextButton  = document.querySelector("#next-button");
-const volumeSlider  = document.querySelector("#volume-slider")
-
-// const playlist = [
-//     {title: " song 1", src: ""}
-// ]
-
 function playTrack(index) {
     playlistitems.forEach((items) => {
         item.classList.remove("active");
@@ -134,36 +112,3 @@ playlistitems.forEach((items, index) => {
     });
 });
 
-// Play/pause button functionality
-playPauseButton.addEventListener("click", () => {
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-        playPauseButton.textContent = "Pause";
-    } else {
-        audioPlayer.pause();
-        playPauseButton.textContent = "Play"
-    }
-});
-
-// Previous button functionality
-prevButton.addEventListener("click", () => {
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = playlistitems.length - 1;
-    }
-    playTrack(currentIndex);
-});
-
-// Next button functionality
-nextButton.addEventListener("click", () => {
-    currentIndex++;
-    if (currentIndex >= playlistitems.length) {
-        currentIndex = 0;
-    }
-    playTrack(currentIndex);
-});
-
-// Volume slider functionality
-volumeSlider.addEventListener("input", () => {
-    audioPlayer.volume = volumeSlider.value;
-});
