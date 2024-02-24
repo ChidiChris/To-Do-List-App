fc7b1520f1398af0a99f3731bd671bfd2be3743f
Chidi Christopher <128011857+ChidiChris@users.noreply.github.com>
2024-02-24 15:13:18 -0800

    Update music.js

--- music.js
+++ music.js
@@ -31,60 +31,6 @@ listBox.addEventListener("click", function(e){
     }
 }, false);
 
-// This next function helps you to store the task on your browser.
-// The task will still be visible even after you refresh your browser.
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const inputBox = document.getElementById('input-box');
-//   const listBox = document.getElementById('list-box');
-
-  // Load tasks from local storage
-    
-  // const savedTasks = localStorage.getItem('tasks');
-  // if (savedTasks) {
-  //   listBox.innerHTML = savedTasks;
-  // }
-
-//   // Add task to list and save to local storage
-//   function addTask() {
-//     if (inputBox.value === '') {
-//       alert("Please write something!");
-//     } else {
-//       const li = document.createElement('li');
-//       li.innerHTML = inputBox.value;
-//       listBox.appendChild(li);
-//       let span = document.createElement('span');
-//       span.innerHTML = '\u00d7';
-//       li.appendChild(span);
-//       inputBox.value = '';
-
-//       // Save task to local storage
-//       saveData();
-//     }
-//   }
-
-  // Save tasks to local storage
-  // function saveData() {
-  //   localStorage.setItem('tasks', listBox.innerHTML);
-  // }
-
-//   // Toggle checked status of task and save to local storage
-//   listBox.addEventListener('click', (e) => {
-//     if (e.target.tagName === 'LI') {
-//       e.target.classList.toggle('checked');
-//       saveData();
-//     } else if (e.target.tagName === 'SPAN') {
-//       e.target.parentElement.remove();
-//       saveData();
-//     }
-//   });
-// });
-
-// // This next function helps to display the data when next we open the browser again.
-
-// function showTask(){
-//     listBox.innerHTML = localStorage.getItem("data");
-// }
-// saveData();
 
 
